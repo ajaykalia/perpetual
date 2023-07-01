3ec6679b3194dd01f4a3cdcb774ef2d765b52f85
ajaykalia <ajay.kalia@gmail.com>
2023-07-01 20:55:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,110 @@
+import os
+import openai
+open_ai_key = os.environ['OPEN_AI_KEY']
+openai.api_key = open_ai_key
+from agent_prompts import prompts
+
+from langchain import OpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationSummaryBufferMemory
+from langchain import PromptTemplate
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage)
+from langchain.memory import ChatMessageHistory
+
+#==========================================
+#set up dicts to store each conversation
+#==========================================
+agents = ['llm1'] # add more when they're ready
+
+global memory_table
+memory_table = {}
+global memory_objects
+memory_objects = {}
+global history_objects
+history_objects = {}
+global memory_chains
+memory_chains = {}
+global conversation_objects
+conversation_objects = {}
+global chat_prompts
+chat_prompts = {}
+global chats
+chats = {}
+
+global llm1_chat
+llm1_chat = ChatOpenAI(
+      openai_api_key=open_ai_key,
+      model_name='gpt-4',
+      #model_name = 'gpt-3.5-turbo',
+      temperature = 0.7,
+      max_tokens = 80
+  )
+chats['llm1'] = llm1_chat
+
+global chat_summary
+chat_summary = ChatOpenAI(
+    openai_api_key=open_ai_key,
+    model_name='gpt-3.5-turbo',
+    temperature = 0.5,
+    max_tokens = 100
+)
+
+for agent in agents:
+  conversation_objects[agent] = {}
+  history_objects[agent] = {}
+  memory_objects[agent] = ConversationSummaryBufferMemory(llm = chat_summary, return_messages = True, max_token_limit=10000)
+  #memory_objects[character] = ConversationBufferMemory()
+
+  system_template=prompts[agent]
+  human_template="{input}"
+  system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+  human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+  chat_prompts[agent] = ChatPromptTemplate.from_messages([system_message_prompt, MessagesPlaceholder(variable_name="history"), human_message_prompt])
+  conversation_objects[agent] = ConversationChain(prompt=chat_prompts[agent], llm=chats[agent], memory = memory_objects[agent], verbose=True)
+  history_objects[agent] = ChatMessageHistory()
+
+#==========================================
+#define each agent
+#==========================================
+def run_llm1(input_text):
+  # LLM1 is an AI that believes it is in a conversation with a human
+  
+  print('=========================')
+  print("activating llm_1")
+  print('=========================')
+  history_objects['llm1'].add_user_message(input_text)
+  
+  resp = conversation_objects['llm1'].predict_and_parse(input=input_text) 
+  history_objects['llm1'].add_ai_message(resp)
+  print("LLM1 (KENDALL_AI):", resp, '\n')
+
+
+#==========================================
+#start the perpetual loop
+#==========================================
+llm1_start = "Just finished acquisition talks for a sick new digital media startup. Really transforming the potential for hyperconnected virtual experiences. We're probably overpaying, but what the hell, gotta ride the lightning."
+
+history_objects['llm1'].add_ai_message(llm1_start)
+memory_objects['llm1'].chat_memory.add_ai_message(llm1_start)
+
+for i in range(1,2):
+    print("============ITERATION ",i)
+    run_llm1("Interesting. Tell me about your dad.")
+
+
+  
+
+      
